55e29ec17176112b71015e9587a652bfbb34a391
kp1129 <katya.p.1129@gmail.com>
2020-01-17 10:06:12 -0700

    code polish, meet mvp

--- components/Header/index.js
+++ components/Header/index.js
@@ -10,23 +10,23 @@
 // And add it to the DOM in the .header-container component
 
 function Header() {
-    // create stuff
-    const mainDiv = document.createElement('div');
-    mainDiv.classList.add('header');
-    const date = document.createElement('span');
-    date.classList.add('date');
-    date.textContent = `MARCH 28, 2019`;
-    const title = document.createElement('h1');
-    title.textContent = 'Lambda Times';
-    const temp = document.createElement('span');
-    temp.classList.add('temp');
-    temp.textContent = `98°`;
-    // assemble stuff
-    mainDiv.append(date);
-    mainDiv.append(title);
-    mainDiv.append(temp);
-    // return stuff
-    return mainDiv;
+  // create
+  const mainDiv = document.createElement("div");
+  mainDiv.classList.add("header");
+  const date = document.createElement("span");
+  date.classList.add("date");
+  date.textContent = `MARCH 28, 2019`;
+  const title = document.createElement("h1");
+  title.textContent = "Lambda Times";
+  const temp = document.createElement("span");
+  temp.classList.add("temp");
+  temp.textContent = `98°`;
+  // assemble
+  mainDiv.append(date);
+  mainDiv.append(title);
+  mainDiv.append(temp);
+  // return
+  return mainDiv;
 }
 
-document.querySelector('.header-container').append(Header());
+document.querySelector(".header-container").append(Header());
