796d57647da27ab63ffaf57cd88369e8fcacda8d
kp1129 <katya.p.1129@gmail.com>
2020-01-17 09:22:12 -0700

    update header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,24 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    // create stuff
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('header');
+    const date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = `MARCH 28, 2019`;
+    const title = document.createElement('h1');
+    title.textContent = 'Lambda Times';
+    const temp = document.createElement('span');
+    temp.classList.add('temp');
+    temp.textContent = `98°`;
+    // assemble stuff
+    mainDiv.append(date);
+    mainDiv.append(title);
+    mainDiv.append(temp);
+    // return stuff
+    return mainDiv;
+}
+
+document.querySelector('.header-container').append(Header());
